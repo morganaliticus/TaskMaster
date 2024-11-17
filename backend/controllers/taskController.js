a4b6c4999cda7527387f7c118943402e02c0dadd
morgan <morganalititcus@gmail.com>
2024-11-17 07:23:28 +0100

    fixed try catch to handle app crshes

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -8,20 +8,25 @@ const Task = require('../models/task');
 */
 
 const createTask = async (req, res) => {
-    const data = req.body;
-    console.log(data);
-
-    //validate priority field
-    if(!["low", "medium", "high"].includes(data.priority)) {
-        return res.status(400).json({
-            error: "'priority' must be one of 'low', 'medium', or 'high'."
-        });
+    try {
+        const data = req.body;
+        console.log(data);
+    
+        //validate priority field
+        if(!["low", "medium", "high"].includes(data.priority)) {
+            return res.status(400).json({
+                error: "'priority' must be one of 'low', 'medium', or 'high'."
+            });
+        }
+    
+        const newTask = new Task(data);
+        await newTask.save();
+    
+        res.status(201).json(newTask);
+    } catch (error) {
+        console.log(`error caught when creating a task = ${error}`);
+        res.status(400).json({error: error.message});
     }
-
-    const newTask = new Task(data);
-    await newTask.save();
-
-    res.status(201).json(newTask);
 }
 
 module.exports = {createTask};
--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const express = require("express");
 const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
+app.use(express.json());
 
 
 const PORT = process.env.PORT || 3000; 
